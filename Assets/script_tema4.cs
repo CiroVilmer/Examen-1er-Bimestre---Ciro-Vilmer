d8c782c377e160a366f844dd0630f29ed7fbe943
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-04-12 15:37:47 -0300

    Finished the test

---
+++ Assets/script_tema4.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_tema4 : MonoBehaviour
+{
+
+    public int cantidadUnidades, cantidadDiasTotales, cantidadDiasLluvia;
+
+    
+    int precioCombustible = 130;
+    int recorridoDia = 90;
+    int recorridoLluvia = 110;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(cantidadDiasTotales < 5 || cantidadDiasLluvia < 0 || cantidadDiasLluvia > cantidadDiasTotales)
+        {
+            Debug.Log("Error, chequee los valores ingresados");
+        }
+        else
+        {
+            int cantidadDiasSinLluvia = cantidadDiasTotales - cantidadDiasLluvia;
+            float recorridoTotal = (recorridoDia * cantidadUnidades) * cantidadDiasSinLluvia + ((recorridoLluvia * cantidadUnidades) * cantidadDiasLluvia);
+            float litrosCombustible = recorridoTotal / 15;
+            float combustibleTotal = litrosCombustible * precioCombustible;
+
+            Debug.Log("Una flota de " + cantidadUnidades + " unidades trabajando durante " + cantidadDiasTotales + " dias implicara un gasto de " + combustibleTotal + " pesos en concepto de combustible");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
